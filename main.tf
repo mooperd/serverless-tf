5829d0afaac526182ed593d6a5089111b212b41d
Andrew Holway <andrew.holway@gmail.com>
2020-09-04 13:55:44 +0200

    fixed perms

--- main.tf
+++ main.tf
@@ -3,32 +3,32 @@ provider "aws" {
 }
 
 locals {
-  org_name     =  "meowtron"
+  org_name = "meowtron"
+  iam_policy_arn = [
+            "arn:aws:iam::aws:policy/AWSLambdaFullAccess", 
+            "arn:aws:iam::aws:policy/IAMFullAccess",
+            "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+            "arn:aws:iam::aws:policy/CloudWatchFullAccess",
+            "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
+            "arn:aws:iam::aws:policy/AWSCloudFormationFullAccess"
+            ]
 }
 
+// Create User
 resource "aws_iam_user" "GitHubUser" {
   name = "GitHubUser"
 }
 
+// Access Key
 resource "aws_iam_access_key" "GitHubUser" {
   user = "${aws_iam_user.GitHubUser.name}"
 }
 
-data "template_file" "policy" {
-  template = "${file("${path.module}/policy.json")}"
-
-  vars = {
-    org_name = "${local.org_name}"
-  }
+resource "aws_iam_user_policy_attachment" "attach-policy" {
+  user       = aws_iam_user.GitHubUser.name
+  count      = "${length(local.iam_policy_arn)}"
+  policy_arn = "${local.iam_policy_arn[count.index]}"
 }
-
-resource "aws_iam_user_policy" "GitHubUser_assume_role" {
-  name = "InstanceManagePolicy"
-  user = "${aws_iam_user.GitHubUser.name}"
-
-  policy = "${data.template_file.policy.rendered}"
-}
-
 
 module "deployment-bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
